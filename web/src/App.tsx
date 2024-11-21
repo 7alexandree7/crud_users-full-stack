4f9a8bb77e518b1961dc68abf289cabc7b195dca
Seu Nome <seuemail@exemplo.com>
2024-11-21 19:46:32 -0300

    Adicionando um novo componente de ChangeTitle

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from "react";
-
-interface Data {
-  message: string;
-}
+import ChangeTitle from "./components/ChangeTitle/ChangeTitle";
 
 const App: React.FC = () => {
-  const [data, setData] = useState<Data | null>(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Simula uma requisição
-    setTimeout(() => {
-      setData({ message: "Dados carregados com sucesso!" });
-      setLoading(false);
-    }, 2000);
-  }, []);
-
-  if (loading) {
-    return <div>Carregando dados...</div>;
-  }
-
-  return (
+  return (  
     <div>
+      <ChangeTitle />
       <h1>Bem-vindo ao App Principal!</h1>
-      <p>{data?.message}</p>
     </div>
   );
 };
---
+++ web/src/components/ChangeTitle/ChangeTitle.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+
+const ChangeTitle = () => {
+    useEffect(() => {
+        const titles = ['Bem-vindo', 'a', 'minha', 'primeira', 'crud', '#neverStorLearning'];
+        let index = 0
+
+        const interval: any = setInterval(() => {
+            document.title = titles[index]
+            index = (index + 1) % titles.length
+        },1000)
+
+        return () => clearInterval(interval)
+    },[])
+
+    return null
+}
+
+
+export default ChangeTitle
