53d8a2bd2fa6a2d3e5781b171425357271001164
Seu Nome <seuemail@exemplo.com>
2024-11-21 19:16:53 -0300

    breakpoint

--- web/src/components/Root/Root.tsx
+++ web/src/components/Root/Root.tsx
@@ -2,19 +2,21 @@ import React, { useState, useEffect } from "react";
 import Loading from "../Loading/Loading.tsx";
 import App from '../../App.tsx';
 
+
 const Root: React.FC = () => {
-    const [isLoading, setIsLoading] = useState(true);
-  
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 2000);
-  
-      return () => clearTimeout(timer); // Limpeza do temporizador
-    }, []);
-  
-    return isLoading ? <Loading /> : <App />;
-  };
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    },2400)
+
+    return () => clearTimeout(timer)
+  },[])
+
+  return isLoading ? <Loading /> : <App />
+}
 
 
 export default Root;
