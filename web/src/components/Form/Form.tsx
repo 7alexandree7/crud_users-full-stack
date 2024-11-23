ee2f6da5b2e55085c7be0b8a6e22f43d9ea1ec83
Seu Nome <seuemail@dominio.com>
2024-11-22 22:56:02 -0300

    avanços no form e adição de nov lib para icons

--- web/src/components/Form/Form.tsx
+++ web/src/components/Form/Form.tsx
@@ -1,76 +1,96 @@
+import { User, Users2 } from "lucide-react";
 import "../../index.css";
 
 function Form() {
   return (
-    <div
-      style={{
-        padding: "1rem",
-        border: "1px solid black",
-        borderRadius: "12px",
-        backgroundColor: "#1A1A1A",
-        color: "#fff",
-      }}
-    >
-      <div style={{ marginBottom: "1rem" }}>
-        <h2>Cadastrar Novo Usuário</h2>
+    <>
+      <div
+        style={{
+          padding: "1rem",
+          borderRadius: "12px",
+          backgroundColor: "#1A1A1A",
+          color: "#fff",
+        }}
+      >
+        <div style={{ marginBottom: "1rem" }}>
+          <h2>Cadastrar Novo Usuário</h2>
+        </div>
+
+        <form>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "0.6rem",
+            }}
+          >
+            <label>
+              Nome:
+              <input
+                className="tt"
+                type="text"
+                name="nome"
+                id="nome"
+                required
+                autoFocus
+              />
+            </label>
+            <label>
+              Idade:
+              <input
+                className="tt"
+                type="number"
+                name="idade"
+                id="idade"
+                required
+              />
+            </label>
+            <label>
+              Profissão
+              <input
+                className="tt"
+                type="text"
+                name="profissao"
+                id="profissao"
+                required
+                style={{
+                  marginBottom: "2rem",
+                }}
+              />
+            </label>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <input className="ss" type="submit" value="Cadastrar" />
+            <input className="ss dd" type="reset" value="Limpar Campos" />
+          </div>
+        </form>
       </div>
 
-      <form>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "0.6rem",
-          }}
-        >
-          <label>
-            Nome:
-            <input
-              className="tt"
-              type="text"
-              name="nome"
-              id="nome"
-              required
-              autoFocus
-            />
-          </label>
-          <label>
-            Idade:
-            <input
-              className="tt"
-              type="number"
-              name="idade"
-              id="idade"
-              required
-            />
-          </label>
-          <label>
-            Profissão
-            <input
-              className="tt"
-              type="text"
-              name="profissao"
-              id="profissao"
-              required
-              style={{
-                marginBottom: "2rem",
-              }}
-            />
-          </label>
-        </div>
-
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <input className="ss" type="submit" value="Cadastrar" />
-          <input className="ss dd"  type="reset" value="Limpar Campos" />
-        </div>
-      </form>
-    </div>
+      <div
+        style={{
+          padding: "1rem",
+          borderRadius: "12px",
+          backgroundColor: "#1A1A1A",
+          color: "#fff",
+          marginTop: "1.5rem",
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <h4>
+          Total de Usuários <span>(0)</span>
+        </h4>
+        <Users2 />
+      </div>
+    </>
   );
 }
 
