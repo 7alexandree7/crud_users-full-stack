7f6ad28ce84c66d17a6878beec679a4067312c30
Seu Nome <seuemail@dominio.com>
2024-11-25 19:08:21 -0300

    atualização no server

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,6 @@
+import express, { json, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
 interface Idata {
     id: any,
     name: string,
@@ -5,15 +8,11 @@ interface Idata {
     profession: string
 }
 
-import express, { json, Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient()
-
 const app = express();
 app.use(express.json())
 
-
+//---------------------------------------------------------------------------------------------------------------------
 app.post('/users', async (req: Request<{}, {}, Idata>, res: Response) => {
     try {
         const { name, age, profession } = req.body;
@@ -26,18 +25,63 @@ app.post('/users', async (req: Request<{}, {}, Idata>, res: Response) => {
                 profession,
             },
         });
+        res.status(201).json(newUser);
+    } 
 
-    res.status(201).json(newUser);
-    } catch (error) {
+    catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Erro ao criar usuário' });
     }
 })
+//---------------------------------------------------------------------------------------------------------------------
+
+app.put('/users/:id', async (req: Request<{ id: string | any }>, res: Response) => {
+    try {
+        const { name, age, profession } = req.body
+
+        const updateUsers = await prisma.user.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                name,
+                age,
+                profession,
+            },
+        })
+
+        res.status(201).json(updateUsers)
+    }
+
+    catch (error) {
+        console.error(error + 'Não foi possivel Editar o usuario');
+        res.status(500).json({ error: 'Erro editar o usuario' });
+    }
+})
+//---------------------------------------------------------------------------------------------------------------------
+
+app.delete('/users/:id', async (req: Request<{ id: string | any }>, res: Response) => {
+    try {
+         await prisma.user.delete({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(200).json({message: 'Usuario deletado com sucesso'})
+    }
+
+    catch (error) {
+        console.error(error + 'Não foi possivel deletar o usuario')
+        res.status(500).json({ error: 'Erro ao deletar um  usuário' });
+    }
+})
+
 
 app.get('/users', async (req: Request, res: Response) => {
     const users = await prisma.user.findMany()
-     res.status(200).json(users);
+    res.status(200).json(users);
 })
+//---------------------------------------------------------------------------------------------------------------------
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
